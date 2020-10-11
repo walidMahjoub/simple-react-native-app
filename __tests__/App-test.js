84af3dc9f32c7f3d2526a87d0dbf73f96f8ae3b9
walidMahjoub <walid.kouissah.mahjoub@gmail.com>
2020-10-11 16:49:31 +0200

    fix app test (#2)

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,22 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import App from 'App'
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist')
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  }
+})
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
